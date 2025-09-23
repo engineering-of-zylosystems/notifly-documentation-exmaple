5eb91e569e2b64d542b3801e990605de18f4aeda
Terri_ <jwkwak1002@gmail.com>
2025-09-23 19:22:39 +0900

    Make docs the main page

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -47,6 +47,7 @@ const config = {
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
+          routeBasePath: '/', // Serve the docs at the site's root
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
@@ -54,6 +55,7 @@ const config = {
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
         blog: {
+          routeBasePath: '/blog', // Keep blog at /blog
           showReadingTime: true,
           feedOptions: {
             type: ['rss', 'atom'],
@@ -71,6 +73,7 @@ const config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        pages: false, // Disable the pages plugin since we're using docs as the main page
       }),
     ],
   ],
